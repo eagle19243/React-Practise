38a0a017482deadacf0c82c49674e8cddd922762
eagle19243 <eagle19243@gmail.com>
2016-12-23 21:36:48 +0100

    React-Demo2 Webpack

---
+++ React-Demo1/src/main.js
@@ -0,0 +1,5 @@
+import React        from 'react';
+import {render}     from 'react-dom';
+import HelloWorld   from './HelloWorld.js';
+
+render(<HelloWorld/>, document.querySelector('#app'));
---
+++ React-Demo2/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <hl> Hello React Skeleton</hl>
+        );
+    }
+}
+
+export default App;
