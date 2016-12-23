ad2b7a83253ff23db29faaa1beeddd4634698754
eagle19243 <eagle19243@gmail.com>
2016-12-23 22:53:44 +0100

    React-Demo3, Create Component, Module

--- React-Demo3/src/components/App.js
+++ React-Demo3/src/components/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-
-let text = 'React-Demo2';
+import Header       from './Header';
+import Content      from './Content';
 
 class App extends React.Component {
-    
-    message() {
-        alert("Hello " + text);
-    }
 
     render() {
-        let pStyle = {
-            color: 'aqua',
-            backgroundColor: 'black'
-        };
-
         return (
             <div>
-                <h1 style = {pStyle}> Hello React Skeleton</h1>
-                <h2 style = {pStyle}> Welcome {text}</h2>
-                <button onClick = {this.message} style = {pStyle}>Click ME</button>
+                <Header />
+                <Content />
             </div>
         );
     }
