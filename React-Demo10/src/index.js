ab0ddf66a7e838c471753b6fec21c56603e7028e
eagle19243 <eagle19243@gmail.com>
2016-12-26 22:23:41 +0100

    React-Demo10, React-Redux

--- React-Demo10/src/index.js
+++ React-Demo10/src/index.js
@@ -1,5 +1,16 @@
-import React            from 'react';
-import ReactDOM         from 'react-dom';
-import {createStore}    from 'redux';
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider  } from 'react-redux';
+import App from './components/App';
+import counterApp from './reducers';
+ 
+const store = createStore(counterApp);
+const rootElement = document.getElementById('root');
+ 
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    rootElement
+);
